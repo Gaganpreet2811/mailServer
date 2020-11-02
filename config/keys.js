a7486dc8369d786f19a3fdf3d08bbb8e48cc4d1d
Gaganpreet2811 <37377426+Gaganpreet2811@users.noreply.github.com>
2020-11-02 19:23:38 +0530

    Add files via upload

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+dbPassword = 'mongodb+srv://Gaganpreet:'+ encodeURIComponent('inkjetpreet635') + '@mycluster.plwtl.mongodb.net/test?retryWrites=true';
+
+module.exports = {
+    mongoURI: dbPassword
+};
